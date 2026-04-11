4d85fe7af7bf0ee298aef951bc6856e1164d8c42
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2026-04-11 18:01:19 +0600

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "This message from controller";
+            return View();
+        }
+    }
+}
