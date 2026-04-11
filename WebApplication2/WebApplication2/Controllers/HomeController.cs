7cb1ab9174033c30089c27f50f17028518af633d
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2026-04-11 21:06:42 +0600

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Student model)
+        {
+            ViewData["Message"]="Your name is " + model.Name + " and your age is " + model.Age;
+            return View();
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        [Required]
+        public required string Name {  get; set; }
+        public int Age { get; set; }
+    }
+}
